2edc559923b39642b439740f60d6cc66c4b2592c
Abdul Karim Khan <your_email@gmail.com>
2026-01-16 00:29:48 +0530

    remove last line

--- Main.java
+++ Main.java
@@ -1,8 +1,5 @@
 public class Main {
       public static void main(String[] args) {
             System.out.println("Helloworld");
-            int a = 4;
-            int b = 23;
-            int sum = a + b;
       }
 }
