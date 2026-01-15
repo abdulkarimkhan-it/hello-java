0976ae486b3677e76f336e94a25d8d33a71d77eb
Abdul Karim Khan <your_email@gmail.com>
2026-01-15 21:44:29 +0530

    Update Main.java output

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
       public static void main(String[] args) {
-            System.out.print("helloword");
+            System.out.println("Helloworld");
+            int a = 4;
+            int b = 23;
       }
 }
